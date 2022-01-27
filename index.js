5c979cefb7d56fffd70cce75fd28248374261383
Marcus McBride <marcusmcb@gmail.com>
2022-01-27 10:14:55 -0800

    code cleanup & notes

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
-const axios = require("axios");
-const cheerio = require("cheerio");
-const fs = require("fs");
+const axios = require('axios')
+const cheerio = require('cheerio')
+const fs = require('fs')
 
-const url = "https://bandcamp.com/djmarcusmcb";
+const url = 'https://bandcamp.com/djmarcusmcb'
 
 async function scrapeData() {
   try {
-    const { data } = await axios.get(url);
-    const $ = cheerio.load(data);
-    const listItems = $(".collection-items ol li");
-    const releases = [];
+    const { data } = await axios.get(url)
+    const $ = cheerio.load(data)
+    const listItems = $('.collection-items ol li')
+    const releases = []
 
     listItems.each((idx, el) => {
-
-      const releaseItem = { title: "", artist: "", artwork: "" }
+      const releaseItem = { title: '', artist: '', artwork: '' }
       // scrape title value
-      let titleVal = $(el).find("div.collection-item-title").first().text()
+      let titleVal = $(el).find('div.collection-item-title').first().text()
       // scrape artist value
-      let artistVal = $(el).find("div.collection-item-artist").first().text()
+      let artistVal = $(el).find('div.collection-item-artist').first().text()
+      // scrape image src
+      let artwork = $(el).find('img.collection-item-art').attr('src')
 
-      let artwork = $(el).find("img.collection-item-art").attr("src")
-      
-      if (titleVal.length != 0 && artistVal.length != 0 && artwork.length !=0) {
+      // check to see if scraped elements are null/undefined
+      // skip them if they are; otherwise, push new obj to array
+
+      if (
+        titleVal.length != 0 &&
+        artistVal.length != 0 &&
+        artwork.length != 0
+      ) {
         titleVal = JSON.stringify(titleVal)
         titleVal = titleVal.split('\\')[0]
         titleVal = titleVal.substring(1)
@@ -29,22 +35,22 @@ async function scrapeData() {
         releaseItem.artwork = artwork
         releaseItem.title = titleVal
         releaseItem.artist = artistVal
-        releases.push(releaseItem);
+        releases.push(releaseItem)
       }
-    });
+    })
 
-    // Write releases array in countries.json file
-    fs.writeFile("releases.json", JSON.stringify(releases, null, 2), (err) => {
+    // Write releases array in releases.json file
+    fs.writeFile('releases.json', JSON.stringify(releases, null, 2), (err) => {
       if (err) {
-        console.error(err);
-        return;
+        console.error(err)
+        return
       }
-      console.log("Successfully written data to file");
+      console.log('Successfully written data to file')
       console.log(releases)
-    });
+    })
   } catch (err) {
-    console.error(err);
+    console.error(err)
   }
 }
 
-scrapeData();
+scrapeData()
