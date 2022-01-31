bcbe58eaa902b74fb4d2d410e6ded87c1bbb9838
Marcus McBride <marcusmcb@gmail.com>
2022-01-30 17:35:23 -0800

    added react, basic front end working

---
+++ client/src/App.js
@@ -0,0 +1,46 @@
+import React, { Fragment, useEffect, useState } from 'react'
+import './App.css'
+
+const App = () => {
+  const [items, setItems] = useState({})
+  const [isBusy, setIsBusy] = useState(true)
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        let req = await fetch('http://localhost:5000')
+        let response = await req.json()        
+        return response
+      } catch (err) {}
+    }
+    getData().then((data) => {
+      setItems(data)
+      setIsBusy(false)
+    })
+  }, [])
+
+  return (
+    <div className='App'>
+      {isBusy ? (
+        <p>Scraping Bandcamp data...</p>
+      ) : (
+        <div className='item-card'>
+          {items.map((item, i) => (
+            <Fragment key={i}>
+              <div>
+                <span>{item.artist} {item.title}</span>   
+                <br />                             
+                <span><a href={item.url}>Link</a></span>               
+                <br/> 
+                <img src={item.artwork} width="300" height="300"></img>     
+                <hr />
+              </div>
+            </Fragment>
+          ))}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const axios = require('axios')
 const cheerio = require('cheerio')
 const fs = require('fs')
+const cors = require('cors')
 
 const app = express()
 const PORT = process.env.PORT || 5000
@@ -63,6 +64,8 @@ async function scrapeData() {
     console.error(err)
   }
 }
+
+app.use(cors())
 
 app.get('/', async (req, res) => {
   let data = await scrapeData()
